9d0198b0f4e11c0b8fd1377afc29d237d00ed24a
Prabhpreet Walia <prabhpreet.business@gmail.com>
2024-05-28 11:43:16 +0530

    .env file created

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-
-const port = 5000
 const app = express()
+
+mongoose.connect(process.env.DB)
 
 app.use(cors)
 app.use(express.json)
@@ -13,4 +14,4 @@ app.get("/", ()=>{
     console.log("hello")
 })
 
-app.listen(port, ()=> console.log(`Server running on port: ${port}`))
+app.listen(process.env.PORT, ()=> console.log(`Server running on port: ${process.env.PORT}`))
